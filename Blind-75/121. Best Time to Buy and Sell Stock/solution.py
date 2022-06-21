91b18cbc0e246cb7410ecae179e3e652bc976a1e
Pranav Bhatia <prav10194@gmail.com>
2022-06-20 20:57:21 -0500

    Create solution.py

---
+++ Blind-75/121. Best Time to Buy and Sell Stock/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        l, r = 0, 1
+        
+        profit = 0
+        
+        while r < len(prices):
+            if prices[l] >= prices[r]:
+                l = r
+                r += 1
+            else:
+                profit = max(profit, prices[r] - prices[l])
+                r += 1
+        return profit
