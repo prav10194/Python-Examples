9828a038fddfb1314d32c925093ce5ab3f003124
Pranav Bhatia <prav10194@gmail.com>
2022-07-17 16:35:38 -0500

    Create solution.py

---
+++ Blind-75/295. Find Median from Data Stream/solution.py
@@ -0,0 +1,38 @@
+class MedianFinder:
+
+    def __init__(self):
+        self.small, self.large = [], []
+        
+
+    def addNum(self, num: int) -> None:
+        
+        #by default add it in smaller max heap
+        heapq.heappush(self.small, -1 * num)
+        
+        # check if value added is greater than any of the values in large min heap
+        if self.small and self.large and (-1 * self.small[0]) > self.large[0]:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+            
+        # check if size is uneven
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small)
+            heapq.heappush(self.large, val)
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large)
+            heapq.heappush(self.small, -1 * val)
+            
+
+    def findMedian(self) -> float:
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        elif len(self.large) > len(self.small):
+            return self.large[0]
+        else:
+            return (-1 * self.small[0] + self.large[0]) / 2
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
