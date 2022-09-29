8afb095900074d23dd197bf873bb4e3ad11dcd9a
Pranav Bhatia <prav10194@gmail.com>
2022-09-28 23:49:03 -0500

    Create solution.py

---
+++ Blind-75/191. Number of 1 Bits/solution.py
@@ -0,0 +1,11 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        
+        res = 0
+        
+        while n:
+            res += n % 2
+            n = n >> 1
+        return res
+        
+        
